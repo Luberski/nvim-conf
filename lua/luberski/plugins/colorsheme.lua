e0774d8e165ee7ba2cb5bb5e4120abd921717645
Mariusz Lubowicki <Luberski@proton.me>
2024-04-03 16:44:08 +0200

    Update colorsheme.lua

--- lua/luberski/plugins/colorsheme.lua
+++ lua/luberski/plugins/colorsheme.lua
@@ -5,6 +5,7 @@ return {
     config = function()
       -- load the colorscheme here
       vim.cmd([[colorscheme rose-pine]])
+      vim.api.nvim_set_hl(0, "Normal:, { bg = "none" })
       vim.api.nvim_set_hl(0, "NormalFloat:, { bg = "none" })
       vim.api.nvim_set_hl(0, "SignColumn:, { bg = "none" })
       vim.api.nvim_set_hl(0, "LineNr:, { bg = "none" })
