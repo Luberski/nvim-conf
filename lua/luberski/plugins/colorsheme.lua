987394a3ce502ac4435758ab15b671bc7db3a930
Mariusz Lubowicki <Luberski@proton.me>
2024-04-03 16:43:48 +0200

    Update colorsheme.lua

--- lua/luberski/plugins/colorsheme.lua
+++ lua/luberski/plugins/colorsheme.lua
@@ -1,10 +1,14 @@
 return {
   {
-    "bluz71/vim-nightfly-guicolors",
+    "will/rose-pine-neovim",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme nightfly]])
+      vim.cmd([[colorscheme rose-pine]])
+      vim.api.nvim_set_hl(0, "NormalFloat:, { bg = "none" })
+      vim.api.nvim_set_hl(0, "SignColumn:, { bg = "none" })
+      vim.api.nvim_set_hl(0, "LineNr:, { bg = "none" })
+      vim.api.nvim_set_hl(0, "CursorLineNr:, { bg = "none" })
     end,
   },
 }
