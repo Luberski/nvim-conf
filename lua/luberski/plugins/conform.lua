344b9a633b8b27520909f4d1fc33652a42ec57f9
Luberski <Luberski@proton.me>
2025-02-26 14:20:55 +0100

    resolve errors / update

--- lua/luberski/plugins/conform.lua
+++ lua/luberski/plugins/conform.lua
@@ -1,42 +1,37 @@
 return {
   "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      ["javascript"] = { "prettier" },
-      ["javascriptreact"] = { "prettier" },
-      ["typescript"] = { "prettier" },
-      ["typescriptreact"] = { "prettier" },
-      ["vue"] = { "prettier" },
-      ["css"] = { "prettier" },
-      ["scss"] = { "prettier" },
-      ["less"] = { "prettier" },
-      ["html"] = { "prettier" },
-      ["json"] = { "prettier" },
-      ["jsonc"] = { "prettier" },
-      ["yaml"] = { "prettier" },
-      ["markdown"] = { "prettier" },
-      ["markdown.mdx"] = { "prettier" },
-      ["graphql"] = { "prettier" },
-      ["handlebars"] = { "prettier" },
-    },
-  },
-
-  config = function()
-    require('conform').setup({
+  opts = function()
+    return {
       formatters_by_ft = {
-        lua = { "stylua" },
-        -- Conform will run multiple formatters sequentially
-        python = { "isort", "black" },
-        -- Use a sub-list to run only the first available formatter
-        javascript = { { "prettierd", "prettier" } },
-        c = { { "clangd" } },
+        ["javascript"] = { "prettierd", "prettier" },
+        ["javascriptreact"] = { "prettierd", "prettier" },
+        ["typescript"] = { "prettierd", "prettier" },
+        ["typescriptreact"] = { "prettierd", "prettier" },
+        ["vue"] = { "prettierd", "prettier" },
+        ["css"] = { "prettier" },
+        ["scss"] = { "prettier" },
+        ["less"] = { "prettier" },
+        ["html"] = { "prettier" },
+        ["json"] = { "prettier" },
+        ["jsonc"] = { "prettier" },
+        ["yaml"] = { "prettier" },
+        ["markdown"] = { "prettier" },
+        ["markdown.mdx"] = { "prettier" },
+        ["graphql"] = { "prettier" },
+        ["handlebars"] = { "prettier" },
+        -- Other languages
+        ["lua"] = { "stylua" },
+        ["python"] = { "isort", "black" },
+        ["c"] = { "clangd" },
       },
 
+
       format_on_save = {
+        stop_after_first = true,
         -- These options will be passed to conform.format()
         timeout_ms = 500,
         lsp_fallback = true,
       },
-    })
-  end
+    }
+  end,
 }
